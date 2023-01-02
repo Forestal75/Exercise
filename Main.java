015e9b93d43473f66bad079b88e96c0c6970143d
Forestal75 <121159400+Forestal75@users.noreply.github.com>
2023-01-02 09:58:59 +0100

    Nesze!

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = new int [20];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = (int) (Math.random() * 100) +1;
+        }
+
+    }
+}
