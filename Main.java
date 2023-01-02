03fb1d3ad9ece97baafb32376d7d6b4ff96ee83c
unknown <lengvari.tamas@gmail.com>
2023-01-02 10:28:28 +0100

    hello

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = (int) (Math.random() * 100) +1;
         }
+        System.out.println("hello");
 
     }
 }
