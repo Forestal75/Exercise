c2a40258e933ae34185c98cc1251bd770453ba68
Forestal75 <121159400+Forestal75@users.noreply.github.com>
2023-01-02 10:39:10 +0100

    minVal method

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] intArray = new int [20];
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = (int) (Math.random() * 100) +1;
         }
-        System.out.println("hello");
+        System.out.println("The number with the minimum value is" + minVal(intArray) + ".");
 
+    }
+
+    public static int minVal(int[] intArray) {
+        System.out.println(Arrays.toString(intArray));
+        int min = intArray[0];
+        for (int i = 0; i < intArray.length; i++) {
+            if (min > intArray[i]) {
+                min = intArray[i];
+            }
+        }
+        return min;
     }
 }
